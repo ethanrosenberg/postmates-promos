461302339a14ff0d8c133d87b6151e3682ab81c3
Ethan Rosenberg <ethan@ethanrosenberg.com>
2019-10-19 16:23:05 -0700

    Added /promos route to promos#all method.

--- app/controllers/promos_controller.rb
+++ app/controllers/promos_controller.rb
@@ -1,2 +1,8 @@
 class PromosController < ApplicationController
+
+  def all
+
+    render json: Promo.all
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :promos
+  #resources :promos
+
+  scope '/api/v1' do
+    get '/promos', to: 'promos#all'
+
+  end
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
